047f5073f8cfbc302c561039b5737a7ff4407279
Mathieu Marc Pobre <mathieu_marc_pobre@dlsu.edu.ph>
2024-05-24 21:28:30 +0800

    Spawn Batch with Input Field

--- ProBuilder/Assets/Scripts/ButtonScript.cs
+++ ProBuilder/Assets/Scripts/ButtonScript.cs
@@ -6,6 +6,8 @@ public class ButtonScript : MonoBehaviour
 {
     [SerializeField] private GameObject template;
     [SerializeField] private List<GameObject> objectList;
+
+    public const string NUM_SPAWN_KEY = "NUM_SPAWN_KEY";
     // Start is called before the first frame update
     void Start()
     {
@@ -24,11 +26,17 @@ public class ButtonScript : MonoBehaviour
         
     }
 
-    private void OnSpawnEvent()
+    private void OnSpawnEvent(Parameters spawn)
     {
-        Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0.0f, Random.Range(-5.0f, 10.0f));
-        GameObject instance = GameObject.Instantiate(this.template,position, Quaternion.identity);
-        instance.SetActive(true);
-        objectList.Add(instance);
+        int numSpawns = spawn.GetIntExtra(NUM_SPAWN_KEY,1);
+
+        for(int i = 0; i < numSpawns; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0.0f, Random.Range(-5.0f, 10.0f));
+            GameObject instance = GameObject.Instantiate(this.template, position, Quaternion.identity);
+            instance.SetActive(true);
+            objectList.Add(instance);
+        }
+       
     }
 }
--- ProBuilder/Assets/Scripts/HudDisplay.cs
+++ ProBuilder/Assets/Scripts/HudDisplay.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 public class HUD : MonoBehaviour
 {
     [SerializeField] private Text spawnText;
+    [SerializeField] private InputField spawnAmount;
     private int spawnCounter = 10;
+    private int NUM_SPAWN = 0;
     // Start is called before the first frame update
     void Start()
     {
         this.spawnText.text = "Available balls: " + this.spawnCounter;
+        
     }
 
     // Update is called once per frame
@@ -21,9 +24,13 @@ public class HUD : MonoBehaviour
 
     public void OnButtonClicked()
     {
+        string input = spawnAmount.text;
+        int.TryParse(input, out NUM_SPAWN);
+        Parameters parameters = new Parameters();
+        parameters.PutExtra(ButtonScript.NUM_SPAWN_KEY, NUM_SPAWN);
         if(spawnCounter != 0)
         {
-            EventBroadcaster.Instance.PostEvent(EventNames.S18_Events.ON_SPAWN_CLICK);
+            EventBroadcaster.Instance.PostEvent(EventNames.S18_Events.ON_SPAWN_CLICK, parameters);
             this.spawnCounter--;
             this.spawnText.text = "Available balls: " + this.spawnCounter;
 
